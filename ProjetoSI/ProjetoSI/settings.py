eda8374cca21171cbfa6b92a78070db10ecad93e
Filipe Wesley <filipe.wlemos@gmail.com>
2016-08-02 13:06:14 -0300

    FIX - Correção no nome do projeto

--- ProjetoSI/ProjetoSI/settings.py
+++ ProjetoSI/ProjetoSI/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'authentication',
+    'ProjetoSI_App',
     # Third-party applications
     'rest_framework',
     'rest_framework.authtoken',
---
+++ ProjetoSI/ProjetoSI_App/urls/user_urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+
+from ProjetoSI_App.views.login_view import *
+from ProjetoSI_App.views.user_view import UserByTokenIDHandler
+
+urlpatterns = [
+    url(r'^rest-auth/login/$', LoginView.as_view(), name='rest_login'),
+
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^rest-auth/user-id/(?P<user_token>\w+)/$', UserByTokenIDHandler.as_view()),
+]
